32bacb1cc1de91e92b643cd547f056c5bdd1ba46
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-04-05 15:18:21 -0600

    added api for tracking

--- src/App.js
+++ src/App.js
@@ -1,34 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 function App() {
+  //hooks
+  const [latest, setLatest] = useState("");
+  useEffect(() => {
+    axios
+      .get("https://corona.lmao.ninja/all")
+      .then((res) => {
+        setLatest(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <CardDeck>
-        <Card bg="secondary" text="white">
+        <Card
+          bg="secondary"
+          text="white"
+          className="text-center"
+          style={{ margin: "10px" }}
+        >
           <Card.Body>
             <Card.Title>Cases</Card.Title>
-            <Card.Text>100</Card.Text>
+            <Card.Text>{latest.cases}</Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card bg="danger" text={"white"}>
+        <Card
+          bg="danger"
+          text={"white"}
+          className="text-center"
+          style={{ margin: "10px" }}
+        >
           <Card.Body>
             <Card.Title>Death</Card.Title>
-            <Card.Text>0</Card.Text>
+  <Card.Text>{latest.deaths}</Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card bg="success" text={"white"}>
+        <Card
+          bg="success"
+          text={"white"}
+          className="text-center"
+          style={{ margin: "10px" }}
+        >
           <Card.Body>
             <Card.Title>Recovered</Card.Title>
-            <Card.Text>0</Card.Text>
+  <Card.Text>{latest.recovered}</Card.Text>
           </Card.Body>
           <Card.Footer>
             <small>Last updated 3 mins ago</small>
